03cb003374fb7afd5c03d6efb4cfedc5798dee4d
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-08 05:44:55 -0400

    Total refactor
    Chasing down every possible thing that can generate CutPlant jobs got old fast; go nuclear and patch the JobMaker directly, that way it shouldn't matter whether I know about it or not.

---
+++ Source/TreeConnectionDeannoyance/HarmonyPatches/HarmonyPatch_JobMaker_MakeJob.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using Verse;
+using Verse.AI;
+
+namespace Cerespirin.TreeDesireDeannoyance
+{
+	[HarmonyPatch(typeof(JobMaker), nameof(JobMaker.MakeJob), new Type[] { typeof(JobDef), typeof(LocalTargetInfo) })]
+	public static class HarmonyPatch_JobMaker_MakeJob 
+	{
+		public static void Postfix(ref Job __result, IntVec3 targetA)
+		{
+			if (!Current.Game.GetComponent<MyGameComponent>().extractTreesAggressively) return;
+
+			if (__result.def == JobDefOf.CutPlant)
+			{
+				if (__result?.targetA.Thing?.def.plant?.IsTree ?? false)
+				{
+					Log.Message("[TreeDesireDeannoyance] HarmonyPatch_JobMaker_MakeJob: Postfix changed job def.");
+					__result.def = JobDefOf.ExtractTree;
+					__result.targetA.Thing.Map.designationManager.AddDesignation(new Designation(__result.targetA.Thing, DesignationDefOf.ExtractTree));
+				}
+			}
+		}
+	}
+}
---
+++ Source/TreeConnectionDeannoyance/HarmonyPatches/HarmonyPatch_Job_GetTargetQueue.cs
@@ -0,0 +1,56 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+using Verse.AI;
+
+namespace Cerespirin.TreeDesireDeannoyance
+{
+	[HarmonyPatch(typeof(Job), nameof(Job.GetTargetQueue), new Type[] { typeof(JobDef) })]
+	public static class HarmonyPatch_Job_GetTargetQueue
+	{
+		public static void Postfix(ref Job __instance)
+		{
+			if (!Current.Game.GetComponent<MyGameComponent>().extractTreesAggressively) return;
+
+			if (__instance.def == JobDefOf.Harvest)
+			{
+				// GetTargetQueue cannot return null, but can return an empty list!
+				LocalTargetInfo firstTarget = __instance.GetTargetQueue(TargetIndex.A).FirstOrFallback(null);
+
+				if (firstTarget == null) return;
+
+				if (firstTarget.Thing.def.plant.IsTree)
+				{
+					// CalculateWantedPlantDef can return null!
+					ThingDef wantedPlantDef = WorkGiver_Grower.CalculateWantedPlantDef(firstTarget.Cell, firstTarget.Thing.Map);
+					IEnumerable<LocalTargetInfo> newQueue = __instance.GetTargetQueue(TargetIndex.A).Where(t => t.Thing.def.plant.IsTree);
+
+					if (wantedPlantDef != null && firstTarget.Thing.def == wantedPlantDef)
+					{
+						newQueue = newQueue.Where(t => t.Thing.def == wantedPlantDef);
+					}
+					else
+					{
+						// This shouldn't need a null check unless there are things without defs, which I don't think exist.
+						newQueue = newQueue.Where(t => t.Thing.def != wantedPlantDef);
+						Log.Message("[TreeDesireDeannoyance] HarmonyPatch_Job_GetTargetQueue: Postfix changed job def.");
+						__instance.def = JobDefOf.ExtractTree;
+						
+						foreach (LocalTargetInfo target in newQueue)
+						{
+							target.Thing.Map.designationManager.AddDesignation(new Designation(target.Thing, DesignationDefOf.ExtractTree));
+						}
+					}
+					__instance.targetQueueA = newQueue.ToList();
+				}
+				else
+				{
+					__instance.targetQueueA = __instance.targetQueueA.Where(t => !t.Thing.def.plant.IsTree).ToList();
+				}
+			}
+		}
+	}
+}
