1c90dd690688a8959e4f80af36abd51414a69ea1
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-08 11:11:12 -0400

    1.6.0.0b (#1)
    * Commented out transpilers
    
    I'm not convinced these are doing what I wanted them to; they certainly don't seem to be working.
    
    * Total refactor
    
    Chasing down every possible thing that can generate CutPlant jobs got old fast; go nuclear and patch the JobMaker directly, that way it shouldn't matter whether I know about it or not.

---
+++ Source/TreeConnectionDeannoyance/HarmonyPatches/HarmonyPatch_WorkGiver_GrowerHarvest_JobOnCell.cs
@@ -0,0 +1,52 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+using Verse.AI;
+
+namespace Cerespirin.TreeDesireDeannoyance
+{
+	[HarmonyPatch(typeof(WorkGiver_GrowerHarvest), nameof(WorkGiver_GrowerHarvest.JobOnCell))]
+	public static class HarmonyPatch_WorkGiver_GrowerHarvest_JobOnCell
+	{
+		public static void Postfix(ref Job __result)
+		{
+			if (!Current.Game.GetComponent<MyGameComponent>().alwaysExtractTrees) return;
+
+			if (__result.def == JobDefOf.Harvest)
+			{
+				// GetTargetQueue cannot return null, but can return an empty list!
+				LocalTargetInfo firstTarget = __result.targetQueueA.FirstOrFallback(null);
+
+				if (firstTarget == null) return;
+
+				if (firstTarget.Thing.def.plant.IsTree)
+				{
+					// CalculateWantedPlantDef can return null!
+					ThingDef wantedPlantDef = WorkGiver_Grower.CalculateWantedPlantDef(firstTarget.Cell, firstTarget.Thing.Map);
+
+					if (wantedPlantDef != null && firstTarget.Thing.def == wantedPlantDef)
+					{
+						__result.targetQueueA = __result.targetQueueA.Where(t => t.Thing.def == wantedPlantDef).ToList();
+					}
+					else
+					{
+						__result.def = JobDefOf.ExtractTree;
+						__result.targetA = firstTarget;
+						__result.targetQueueA = null;
+
+						if (!firstTarget.Thing.Map.designationManager.HasMapDesignationOn(firstTarget.Thing))
+						{
+							firstTarget.Thing.Map.designationManager.AddDesignation(new Designation(firstTarget.Thing, DesignationDefOf.ExtractTree));
+						}
+					}
+				}
+				else
+				{
+					__result.targetQueueA = __result.targetQueueA.Where(t => !t.Thing.def.plant.IsTree).ToList();
+				}
+			}
+		}
+	}
+}
--- Source/TreeConnectionDeannoyance/MyGameComponent.cs
+++ Source/TreeConnectionDeannoyance/MyGameComponent.cs
@@ -4,13 +4,13 @@ namespace Cerespirin.TreeDesireDeannoyance
 {
 	public class MyGameComponent : GameComponent
 	{
-		public bool extractTreesAggressively = false;
+		public bool alwaysExtractTrees = false;
 
 		public MyGameComponent(Game game) { }
 
 		public override void ExposeData()
 		{
-			Scribe_Values.Look(ref extractTreesAggressively, "TreeDesireDeannoyance_ExtractTreesAggressively");
+			Scribe_Values.Look(ref alwaysExtractTrees, "TreeDesireDeannoyance_ExtractTreesAggressively");
 		}
 	}
 }
