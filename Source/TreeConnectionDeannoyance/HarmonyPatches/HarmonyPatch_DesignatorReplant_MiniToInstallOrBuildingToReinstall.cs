0ec9956b4f1d11ec84884f57dd5f2d1220a54e08
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-09 14:10:45 -0400

    Update HarmonyPatch_DesignatorReplant_MiniToInstallOrBuildingToReinstall.cs

--- Source/TreeConnectionDeannoyance/HarmonyPatches/HarmonyPatch_DesignatorReplant_MiniToInstallOrBuildingToReinstall.cs
+++ Source/TreeConnectionDeannoyance/HarmonyPatches/HarmonyPatch_DesignatorReplant_MiniToInstallOrBuildingToReinstall.cs
@@ -1,6 +1,5 @@
 ﻿using HarmonyLib;
 using RimWorld;
-using System.Collections.Generic;
 using System.Reflection;
 using Verse;
 
@@ -9,9 +8,9 @@ namespace Cerespirin.TreeDesireDeannoyance
 	[HarmonyPatch]
 	public static class HarmonyPatch_DesignatorReplant_MiniToInstallOrBuildingToReinstall
 	{
-		public static IEnumerable<MethodInfo> TargetMethods()
+		public static MethodBase TargetMethod()
 		{
-			yield return typeof(Designator_Replant).GetProperty("MiniToInstallOrBuildingToReinstall", BindingFlags.NonPublic | BindingFlags.Instance).GetGetMethod();
+			return typeof(Designator_Replant).GetProperty("MiniToInstallOrBuildingToReinstall", BindingFlags.NonPublic | BindingFlags.Instance).GetGetMethod();
 		}
 
 		public static bool Prefix(ref Thing __result, Gizmo __instance)
