a2ed0100f3bc16bb2dc526782c3fc272e963426d
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-09 13:23:50 -0400

    Create HarmonyPatch_DesignatorReplant_MiniToInstallOrBuildingToReinstall.cs
    I can't believe designators get their thing references from what you've currently clicked on...

---
+++ Source/TreeConnectionDeannoyance/HarmonyPatches/HarmonyPatch_DesignatorReplant_MiniToInstallOrBuildingToReinstall.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Reflection;
+using Verse;
+using Verse.AI;
+
+namespace Cerespirin.TreeDesireDeannoyance
+{
+	[HarmonyPatch]
+	public static class HarmonyPatch_DesignatorReplant_MiniToInstallOrBuildingToReinstall
+	{
+		public static IEnumerable<MethodInfo> TargetMethods()
+		{
+			yield return typeof(Designator_Replant).GetProperty("MiniToInstallOrBuildingToReinstall", BindingFlags.NonPublic | BindingFlags.Instance).GetGetMethod();
+		}
+
+		public static bool Prefix()
+		{
+			return true;
+		}
+	}
+}
