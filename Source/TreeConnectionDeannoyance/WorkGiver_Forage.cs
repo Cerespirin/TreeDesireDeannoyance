f9a26a9eda973d6f1ea7f323481490654a5d4e90
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-09 21:44:21 -0400

    Create WorkGiver_Forage.cs

---
+++ Source/TreeConnectionDeannoyance/WorkGiver_Forage.cs
@@ -0,0 +1,42 @@
+﻿using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace Cerespirin.TreeDesireDeannoyance
+{
+	public class WorkGiver_Forage : WorkGiver_PlantsCut
+	{
+		public override ThingRequest PotentialWorkThingRequest
+		{
+			get 
+			{
+				return ThingRequest.ForGroup(ThingRequestGroup.Plant);
+			}
+		}
+
+		public override bool ShouldSkip(Pawn pawn, bool forced = false)
+		{
+			if (pawn.Map.areaManager.GetLabeled("Forage") == null)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		public override Job JobOnThing(Pawn pawn, Thing t, bool forced = false)
+		{
+			if (t.def.plant?.isStump ?? false)
+			{
+				if (pawn.Map.areaManager.GetLabeled("Forage") != null) 
+				{
+					if (t.Map.designationManager.HasMapDesignationOn(t))
+					{
+						t.Map.designationManager.AddDesignation(new Designation(t, DesignationDefOf.CutPlant));
+					}
+					return base.JobOnThing(pawn, t, forced);
+				}
+			}
+			return null;
+		}
+	}
+}
