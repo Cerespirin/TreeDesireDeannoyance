b6a0551e153fbb39362abc80b28811a891e5c65c
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-12 05:22:07 -0400

    Update HarmonyPatch_JobMaker_MakeJob.cs

--- Source/TreeConnectionDeannoyance/HarmonyPatches/HarmonyPatch_JobMaker_MakeJob.cs
+++ Source/TreeConnectionDeannoyance/HarmonyPatches/HarmonyPatch_JobMaker_MakeJob.cs
@@ -7,7 +7,7 @@ using Verse.AI;
 namespace Cerespirin.TreeDesireDeannoyance
 {
 	[HarmonyPatch(typeof(JobMaker), nameof(JobMaker.MakeJob), new Type[] { typeof(JobDef), typeof(LocalTargetInfo) })]
-	public static class HarmonyPatch_JobMaker_MakeJob 
+	public static class HarmonyPatch_JobMaker_MakeJob
 	{
 		public static void Postfix(ref Job __result, IntVec3 targetA)
 		{
