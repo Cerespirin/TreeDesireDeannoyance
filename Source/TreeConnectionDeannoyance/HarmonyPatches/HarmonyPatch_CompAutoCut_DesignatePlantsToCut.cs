a88012a8d028e9eb08d66cdd055f617e2984c668
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-11 23:24:37 -0400

    Create HarmonyPatch_CompAutoCut_DesignatePlantsToCut.cs

---
+++ Source/TreeConnectionDeannoyance/HarmonyPatches/HarmonyPatch_CompAutoCut_DesignatePlantsToCut.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+
+namespace Cerespirin.TreeDesireDeannoyance
+{
+	[HarmonyPatch(typeof(CompAutoCut), nameof(CompAutoCut.DesignatePlantsToCut)]
+	public static class HarmonyPatch_CompAutoCut_DesignatePlantsToCut
+	{
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
