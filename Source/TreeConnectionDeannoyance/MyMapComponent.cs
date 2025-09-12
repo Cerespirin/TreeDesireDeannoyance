bbd47d210e4da5995cd6a2119ed7aed623202660
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-11 22:40:10 -0400

    Create MyMapComponent.cs

---
+++ Source/TreeConnectionDeannoyance/MyMapComponent.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace Cerespirin.TreeDesireDeannoyance
+{
+	public class MyMapComponent : MapComponent
+	{
+		public MyMapComponent(Game game) : base(map) { }
+	}
+}
