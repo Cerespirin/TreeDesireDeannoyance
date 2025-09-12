a05779e086e17129dd8a067ac80655a29b47a0d1
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-11 22:54:56 -0400

    Update MyMapComponent.cs

--- Source/TreeConnectionDeannoyance/MyMapComponent.cs
+++ Source/TreeConnectionDeannoyance/MyMapComponent.cs
@@ -6,5 +6,14 @@ namespace Cerespirin.TreeDesireDeannoyance
 	public class MyMapComponent : MapComponent
 	{
 		public MyMapComponent(Map map) : base(map) { }
+
+		public override void MapComponentTick()
+		{
+			// Long tick
+			if (Gen.IsHashIntervalTick(map, 2000))
+			{
+
+			}
+		}
 	}
 }
