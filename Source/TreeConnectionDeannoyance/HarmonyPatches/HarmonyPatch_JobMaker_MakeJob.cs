60884fbd781b736d16083e1651f3759ece2609dc
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-12 17:37:10 -0400

    Update HarmonyPatch_JobMaker_MakeJob.cs

--- Source/TreeConnectionDeannoyance/HarmonyPatches/HarmonyPatch_JobMaker_MakeJob.cs
+++ Source/TreeConnectionDeannoyance/HarmonyPatches/HarmonyPatch_JobMaker_MakeJob.cs
@@ -9,7 +9,7 @@ namespace Cerespirin.TreeDesireDeannoyance
 	[HarmonyPatch(typeof(JobMaker), nameof(JobMaker.MakeJob), new Type[] { typeof(JobDef), typeof(LocalTargetInfo) })]
 	public static class HarmonyPatch_JobMaker_MakeJob
 	{
-		public static void Postfix(ref Job __result, IntVec3 targetA)
+		public static void Postfix(ref Job __result)
 		{
 			if (!Current.Game.GetComponent<MyGameComponent>().alwaysExtractTrees) return;
 
