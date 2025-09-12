ba9aa7dac10817785227169d9a3522b7954d0525
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-12 04:58:34 -0400

    CompAutoCut support (#7)
    As used by pen markers and wind turbines.
    
    * Create HarmonyPatch_CompAutoCut_DesignatePlantsToCut.cs
    
    * Update MyHelper.cs

---
+++ Source/TreeConnectionDeannoyance/HarmonyPatches/HarmonyPatch_CompAutoCut_DesignatePlantsToCut.cs
@@ -0,0 +1,53 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using Verse;
+
+namespace Cerespirin.TreeDesireDeannoyance
+{
+	[HarmonyPatch(typeof(CompAutoCut), nameof(CompAutoCut.DesignatePlantsToCut))]
+	public static class HarmonyPatch_CompAutoCut_DesignatePlantsToCut
+	{
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+		{
+			List<CodeInstruction> instructionsAsList = instructions.ToList();
+			LocalBuilder extractSetting = generator.DeclareLocal(typeof(bool));
+
+			foreach (CodeInstruction instruction in instructionsAsList)
+			{
+				if (instruction.opcode == OpCodes.Stloc_0)
+				{
+					yield return instruction;
+					yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(MyHelper), nameof(MyHelper.GetExtractSetting)));
+					yield return new CodeInstruction(OpCodes.Stloc_S, extractSetting.LocalIndex);
+				}
+				else if (instruction.opcode == OpCodes.Ldsfld && (FieldInfo)instruction.operand == AccessTools.Field(typeof(DesignationDefOf), nameof(DesignationDefOf.CutPlant)))
+				{
+					// Overwrite this instruction with our own.
+					yield return new CodeInstruction(OpCodes.Ldloc_S, 4);
+					yield return new CodeInstruction(OpCodes.Ldloc_S, extractSetting.LocalIndex);
+					yield return new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(HarmonyPatch_CompAutoCut_DesignatePlantsToCut), nameof(GetAppropriateDesignation)));
+				}
+				else
+				{
+					yield return instruction;
+				}
+			}
+		}
+
+		public static DesignationDef GetAppropriateDesignation(Thing plant, bool alwaysExtract)
+		{
+			if (alwaysExtract && plant.def.plant.IsTree && plant.def.plant.treeLoversCareIfChopped)
+			{
+				return DesignationDefOf.ExtractTree;
+			}
+			else
+			{
+				return DesignationDefOf.CutPlant;
+			}
+		}
+	}
+}
