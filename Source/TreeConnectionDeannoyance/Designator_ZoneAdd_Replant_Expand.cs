4fb7a607389523a4ff5a8d0f9d58d0c0ee0c0a6c
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-15 07:18:55 -0400

    Create Designator_ZoneAdd_Replant_Expand

---
+++ Source/TreeConnectionDeannoyance/Designator_ZoneAdd_Replant_Expand.cs
@@ -0,0 +1,22 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Cerespirin.TreeDesireDeannoyance
+{
+	public class Designator_ZoneAdd_Replant_Expand : Designator_ZoneAdd_Replant
+	{
+		protected override bool ShowRightClickHideOptions
+		{
+			get
+			{
+				return false;
+			}
+		}
+
+		public Designator_ZoneAdd_Replant_Expand()
+		{
+			this.defaultLabel = "DesignatorZoneExpand".Translate();
+			this.hotKey = KeyBindingDefOf.Misc6;
+		}
+	}
+}
