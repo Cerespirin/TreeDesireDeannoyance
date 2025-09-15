f217ce7c09635619c43d9d25c5e10bcc3fdfb79d
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-15 06:43:15 -0400

    Created Zone, Designator

---
+++ Source/TreeConnectionDeannoyance/Designator_ZoneAdd_Replant.cs
@@ -0,0 +1,25 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+
+namespace Cerespirin.TreeDesireDeannoyance
+{
+	public class Designator_ZoneAdd_Replant : Designator_ZoneAdd
+	{
+		protected override string NewZoneLabel
+		{
+			get
+			{
+				return "TreeDesireDeannoyance_ZoneReplant".Translate();
+			}
+		}
+
+		protected override Zone MakeNewZone()
+		{
+			return new Zone_AutoReplant(Find.CurrentMap.zoneManager);
+		}
+	}
+}
---
+++ Source/TreeConnectionDeannoyance/Zone_AutoReplant.cs
@@ -0,0 +1,24 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+
+namespace Cerespirin.TreeDesireDeannoyance
+{
+	public class Zone_AutoReplant : Zone
+	{
+		public Zone_AutoReplant(ZoneManager zoneManager) : base("TreeDesireDeannoyance_ZoneReplant".Translate(), zoneManager)
+		{
+		}
+
+		protected override Color NextZoneColor
+		{
+			get
+			{
+				return Color.Lerp(new Color(0f, 1f, 1f), Color.gray, 0.5f);
+			}
+		}
+	}
+}
