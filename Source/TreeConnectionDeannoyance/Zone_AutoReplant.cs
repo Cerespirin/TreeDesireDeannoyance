3eac1021787e09d0322e74a6bc321a880080ec0a
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-15 10:56:02 -0400

    Update Zone_AutoReplant.cs

--- Source/TreeConnectionDeannoyance/Zone_AutoReplant.cs
+++ Source/TreeConnectionDeannoyance/Zone_AutoReplant.cs
@@ -20,5 +20,17 @@ namespace Cerespirin.TreeDesireDeannoyance
 				return Color.Lerp(Random.ColorHSV(0f, 0.196f, 1, 1, 1, 1), Color.gray, 0.5f).WithAlpha(0.09f);
 			}
 		}
+
+		public override IEnumerable<Gizmo> GetGizmos() 
+		{
+			yield return new Command_Hide_ZoneReplant(this);
+			foreach (Gizmo gizmo in base.GetGizmos()) { yield return gizmo; }
+		}
+
+		public override IEnumerable<Gizmo> GetZoneAddGizmos()
+		{
+			yield return DesignatorUtility.FindAllowedDesignator<Designator_ZoneAdd_Replant_Expand>();
+			yield break;
+		}
 	}
 }
