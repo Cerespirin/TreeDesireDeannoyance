a67cd02861104133ee24547807f4c0b30f87d451
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-16 19:34:44 -0400

    Create WorkGiver_ReplantClear

---
+++ Source/TreeConnectionDeannoyance/WorkGiver_ReplantClear.cs
@@ -0,0 +1,30 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+using Verse.AI;
+using Verse.AI.Group;
+
+namespace Cerespirin.TreeDesireDeannoyance
+{
+	public class WorkGiver_ReplantClear : WorkGiver_ReplantGrowBase
+	{
+		public override PathEndMode PathEndMode => PathEndMode.ClosestTouch;
+		
+		public override bool HasJobOnCell(Pawn pawn, IntVec3 c, bool forced = false)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override bool ShouldSkip(Pawn pawn, bool forced = false)
+		{
+			return pawn.GetLord() != null || base.ShouldSkip(pawn, forced);
+		}
+
+		public override Job JobOnCell(Pawn pawn, IntVec3 c, bool forced = false)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
