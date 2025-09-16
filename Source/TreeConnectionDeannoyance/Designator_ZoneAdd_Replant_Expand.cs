cd61f2fe326062d8684ea6f30958482aeee7f12b
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-15 23:48:55 -0400

    General cleanup

--- Source/TreeConnectionDeannoyance/Designator_ZoneAdd_Replant_Expand.cs
+++ Source/TreeConnectionDeannoyance/Designator_ZoneAdd_Replant_Expand.cs
@@ -5,18 +5,12 @@ namespace Cerespirin.TreeDesireDeannoyance
 {
 	public class Designator_ZoneAdd_Replant_Expand : Designator_ZoneAdd_Replant
 	{
-		protected override bool ShowRightClickHideOptions
-		{
-			get
-			{
-				return false;
-			}
-		}
+		protected override bool ShowRightClickHideOptions => false;
 
 		public Designator_ZoneAdd_Replant_Expand()
 		{
-			this.defaultLabel = "DesignatorZoneExpand".Translate();
-			this.hotKey = KeyBindingDefOf.Misc6;
+			defaultLabel = "DesignatorZoneExpand".Translate();
+			hotKey = KeyBindingDefOf.Misc6;
 		}
 	}
 }
