0389443e2d8a247a1314d814841d0158e65ef4f9
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-16 06:41:56 -0400

    Create WorkGiver_ReplantHarvest

---
+++ Source/TreeConnectionDeannoyance/WorkGiver_ReplantHarvest.cs
@@ -0,0 +1,13 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+using Verse.AI;
+
+namespace Cerespirin.TreeDesireDeannoyance
+{
+	public class WorkGiver_ReplantHarvest : WorkGiver_GrowerHarvest
+	{
+
+	}
+}
