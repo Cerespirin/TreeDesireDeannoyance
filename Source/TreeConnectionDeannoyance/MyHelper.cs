8601e1fe46a123a47a8bfaa2833e893e70ce8d1e
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-15 21:22:06 -0400

    Update MyHelper.cs

--- Source/TreeConnectionDeannoyance/MyHelper.cs
+++ Source/TreeConnectionDeannoyance/MyHelper.cs
@@ -5,6 +5,7 @@ namespace Cerespirin.TreeDesireDeannoyance
 	public static class MyHelper
 	{
 		public static bool IsRelevantToTreeLovers(this Thing thing) => thing.def.plant.IsTree && thing.def.plant.treeLoversCareIfChopped;
+		public static bool IsRelevantToTreeLovers(this ThingDef def) => def.plant.IsTree && def.plant.treeLoversCareIfChopped;
 		public static bool GetExtractSetting() => Current.Game.GetComponent<MyGameComponent>().alwaysExtractTrees;
 		public static bool GetReplantSetting() => Current.Game.GetComponent<MyGameComponent>().autoReplantTrees;
 		public static Area GetReplantArea(this Map map) => map.areaManager.GetLabeled("Replant");
