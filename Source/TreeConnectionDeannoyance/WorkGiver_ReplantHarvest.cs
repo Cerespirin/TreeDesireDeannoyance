7307ef2f39dbd773b45bb6482b341be78780fec3
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-16 18:50:16 -0400

    Update WorkGiver_ReplantHarvest.cs

--- Source/TreeConnectionDeannoyance/WorkGiver_ReplantHarvest.cs
+++ Source/TreeConnectionDeannoyance/WorkGiver_ReplantHarvest.cs
@@ -11,6 +11,11 @@ namespace Cerespirin.TreeDesireDeannoyance
 	public class WorkGiver_ReplantHarvest : WorkGiver_ReplantGrowBase
 	{
 		public override PathEndMode PathEndMode => PathEndMode.Touch;
+		
+		public override bool HasJobOnCell(Pawn pawn, IntVec3 c, bool forced = false)
+		{
+			throw new NotImplementedException();
+		}
 
 		public override Job JobOnCell(Pawn pawn, IntVec3 c, bool forced = false)
 		{
