e0fe817c6b90096e0d4527874b82d34a3b0cfa21
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-15 22:36:00 -0400

    Update MyHelper.cs

--- Source/TreeConnectionDeannoyance/MyHelper.cs
+++ Source/TreeConnectionDeannoyance/MyHelper.cs
@@ -7,7 +7,7 @@ namespace Cerespirin.TreeDesireDeannoyance
 		public static bool ExtractSetting => Current.Game.GetComponent<MyGameComponent>().alwaysExtractTrees;
 		public static bool ReplantSetting => Current.Game.GetComponent<MyGameComponent>().autoReplantTrees;
 		public static bool IsRelevantToTreeLovers(this Thing thing) => thing.def.plant.IsTree && thing.def.plant.treeLoversCareIfChopped;
-		public static bool IsRelevantToTreeLovers(this ThingDef def) => (def.plant?.IsTree ?? false) && (def.plant?.treeLoversCareIfChopped ?? false);
+		public static bool IsRelevantToTreeLovers(this ThingDef def) => def.plant.IsTree && def.plant.treeLoversCareIfChopped;
 		public static Area GetReplantArea(this Map map) => map.areaManager.GetLabeled("Replant");
 		public static Area GetForageArea(this Map map) => map.areaManager.GetLabeled("Forage");
 	}
