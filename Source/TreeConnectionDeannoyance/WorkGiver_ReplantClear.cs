04df0b8bd6dcfd33d0b82ef71d4ee822a399cd40
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-16 20:25:36 -0400

    Update WorkGiver_ReplantClear.cs

--- Source/TreeConnectionDeannoyance/WorkGiver_ReplantClear.cs
+++ Source/TreeConnectionDeannoyance/WorkGiver_ReplantClear.cs
@@ -24,7 +24,33 @@ namespace Cerespirin.TreeDesireDeannoyance
 
 		public override Job JobOnCell(Pawn pawn, IntVec3 c, bool forced = false)
 		{
-			throw new NotImplementedException();
+			Map map = pawn.Map;
+			Zone_Replant zone = c.GetZone(map) as Zone_Replant;
+			Plant plant = c.GetPlant(map);
+
+			foreach (Thing thing in c.GetThingList(map))
+			{
+				if (!thing.def.BlocksPlanting()) /**************/ { continue; }
+				if (!pawn.CanReserve(thing, 1, -1, null, forced)) { return null; }
+
+				if (thing.def.category == ThingCategory.Plant)
+				{
+					if (thing.IsForbidden(pawn)) /**********************************************************/ { return null; }
+					if (zone != null && !zone.allowCut) /***************************************************/ { return null; }
+					if (!forced && plant.TryGetComp(out CompPlantPreventCutting comp) && comp.PreventCutting) { return null; }
+					if (!PlantUtility.PawnWillingToCutPlant_Job(thing, pawn)) /*****************************/ { return null; }
+					// Would return null in GrowerSow but, honestly, as long as we're here we might as well extract the fucking tree.
+					if (PlantUtility.TreeMarkedForExtraction(thing)) /**************************************/ { return JobMaker.MakeJob(JobDefOf.ExtractTree, thing); }
+
+					return JobMaker.MakeJob(JobDefOf.CutPlant, thing);
+				}
+				if (thing.def.EverHaulable) 
+				{ 
+					return HaulAIUtility.HaulAsideJobFor(pawn, thing); 
+				}
+				return null;
+			}
+			return null;
 		}
 	}
 }
