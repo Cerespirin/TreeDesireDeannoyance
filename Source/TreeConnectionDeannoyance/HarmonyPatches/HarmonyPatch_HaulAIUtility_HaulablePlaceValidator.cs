e1817ce7f4f7b3f5cb60c072c56975ea8036991b
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-17 19:42:02 -0400

    Hauling fix?
    This was alarmingly simple to fix, I feel like an idiot.

--- Source/TreeConnectionDeannoyance/HarmonyPatches/HarmonyPatch_HaulAIUtility_HaulablePlaceValidator.cs
+++ Source/TreeConnectionDeannoyance/HarmonyPatches/HarmonyPatch_HaulAIUtility_HaulablePlaceValidator.cs
@@ -9,9 +9,9 @@ using Verse.AI;
 namespace Cerespirin.TreeDesireDeannoyance
 {
 	[HarmonyPatch(typeof(HaulAIUtility), "HaulablePlaceValidator")]
+	[HarmonyDebug]
 	public static class HarmonyPatch_HaulAIUtility_HaulablePlaceValidator
 	{
-		/*
 		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
 		{
 			LocalBuilder newLocal = generator.DeclareLocal(typeof(bool));
@@ -27,7 +27,7 @@ namespace Cerespirin.TreeDesireDeannoyance
 					yield return new CodeInstruction(OpCodes.Ldloc_S, newLocal.LocalIndex);
 					yield return instruction;
 				}
-				if (state == 1 && instruction.opcode == OpCodes.Brfalse_S)
+				else if (state == 1 && instruction.opcode == OpCodes.Brfalse_S)
 				{
 					state = 2;
 					yield return new CodeInstruction(OpCodes.Brtrue_S, newLabel);
@@ -46,6 +46,5 @@ namespace Cerespirin.TreeDesireDeannoyance
 				Log.Error("[TreeDesireDeannoyance] HarmonyPatch_HaulAIUtility_HaulablePlaceValidator: unable to find injection point. This was likely due to a mod incompatibility; please report this to the mod author.");
 			}
 		}
-		*/
 	}
 }
