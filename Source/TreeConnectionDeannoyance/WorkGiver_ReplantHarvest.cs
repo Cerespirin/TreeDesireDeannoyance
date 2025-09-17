ed44b33f286e42acb726bf5df01cd42431166986
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-16 23:05:15 -0400

    n

--- Source/TreeConnectionDeannoyance/WorkGiver_ReplantHarvest.cs
+++ Source/TreeConnectionDeannoyance/WorkGiver_ReplantHarvest.cs
@@ -14,7 +14,15 @@ namespace Cerespirin.TreeDesireDeannoyance
 
 		public override bool HasJobOnCell(Pawn pawn, IntVec3 c, bool forced = false)
 		{
-			throw new NotImplementedException();
+			Plant plant = c.GetPlant(pawn.Map);
+			if (plant == null) /**********************************************************************************************************/ { return false; }
+			if (!plant.HarvestableNow || plant.LifeStage != PlantLifeStage.Mature) /******************************************************/ { return false; }
+			if (!forced && plant.TryGetComp(out CompPlantPreventCutting compPlantPreventCutting) && compPlantPreventCutting.PreventCutting) { return false; }
+			if (!plant.CanYieldNow()) /***************************************************************************************************/ { return false; }
+			if (!plant.def.plant.autoHarvestable && !forced) /****************************************************************************/ { return false; }
+
+			Zone_Replant zone = c.GetZone(pawn.Map) as Zone_Replant;
+			return (zone?.allowCut ?? true) && PlantUtility.PawnWillingToCutPlant_Job(plant, pawn) && pawn.CanReserve(plant, 1, -1, null, forced);
 		}
 
 		public override bool ShouldSkip(Pawn pawn, bool forced = false)
@@ -24,12 +32,32 @@ namespace Cerespirin.TreeDesireDeannoyance
 
 		public override Job JobOnCell(Pawn pawn, IntVec3 c, bool forced = false)
 		{
-			throw new NotImplementedException();
-		}
-
-		protected override bool ExtraRequirements(Zone_Replant zone, Pawn pawn)
-		{
-			throw new NotImplementedException();
+			Job job = JobMaker.MakeJob(JobDefOf.Harvest);
+			Map map = pawn.Map;
+			Room room = c.GetRoom(map);
+			float totalWork = 0f;
+			for (int i = 0; i < 40; i++)
+			{
+				IntVec3 radialCell = c + GenRadial.RadialPattern[i];
+				if (radialCell.GetRoom(map) == room && HasJobOnCell(pawn, radialCell, forced))
+				{
+					Plant plant = radialCell.GetPlant(map);
+					if (!(radialCell != c) || ((radialCell.GetZone(map) as Zone_Replant)?.ReplantFilter.Allows(plant) ?? false)) //plant.def == WorkGiver_Grower.CalculateWantedPlantDef(radialCell, map))
+					{
+						totalWork += plant.def.plant.harvestWork;
+						if (radialCell != c && totalWork > 2400f)
+						{
+							break;
+						}
+						job.AddQueuedTarget(TargetIndex.A, plant);
+					}
+				}
+			}
+			if (job.targetQueueA != null && job.targetQueueA.Count >= 3)
+			{
+				job.targetQueueA.SortBy(targ => targ.Cell.DistanceToSquared(pawn.Position));
+			}
+			return job;
 		}
 	}
 }
