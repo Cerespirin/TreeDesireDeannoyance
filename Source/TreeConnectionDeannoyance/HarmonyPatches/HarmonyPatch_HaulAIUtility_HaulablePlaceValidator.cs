4ed2a3b7ecd4ee0de82a4f6e1e48eec84c204d64
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-17 01:20:08 -0400

    Update HarmonyPatch_HaulAIUtility_HaulablePlaceValidator.cs

--- Source/TreeConnectionDeannoyance/HarmonyPatches/HarmonyPatch_HaulAIUtility_HaulablePlaceValidator.cs
+++ Source/TreeConnectionDeannoyance/HarmonyPatches/HarmonyPatch_HaulAIUtility_HaulablePlaceValidator.cs
@@ -1,8 +1,10 @@
 ﻿using HarmonyLib;
 using RimWorld;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Emit;
+using Verse;
 using Verse.AI;
 
 namespace Cerespirin.TreeDesireDeannoyance
@@ -12,28 +14,34 @@ namespace Cerespirin.TreeDesireDeannoyance
 	{
 		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
 		{
-			List<CodeInstruction> instructionsAsList = instructions.ToList();
 			LocalBuilder extractSetting = generator.DeclareLocal(typeof(bool));
 			object lastBranchLabel = null;
+			bool didTheThing = false;
 
-			foreach (CodeInstruction instruction in instructionsAsList)
+			foreach (CodeInstruction instruction in instructions)
 			{
 				if (instruction.opcode == OpCodes.Brfalse_S)
 				{
 					lastBranchLabel = instruction.operand;
 				}
-				if (instruction.opcode == OpCodes.Isinst && instruction.operand is Zone_Growing)
+				if (instruction.opcode == OpCodes.Isinst && (Type)instruction.operand == typeof(Zone_Growing))
 				{
+					didTheThing = true;
 					yield return new CodeInstruction(OpCodes.Stloc_S, extractSetting.LocalIndex);
 					yield return instruction;
 					yield return new CodeInstruction(OpCodes.Brfalse_S, lastBranchLabel);
 					yield return new CodeInstruction(OpCodes.Ldloc_S, extractSetting.LocalIndex);
 					yield return new CodeInstruction(OpCodes.Isinst, typeof(Zone_Replant));
+					continue;
 				}
 				else
 				{
 					yield return instruction;
 				}
+			}
+			if (!didTheThing)
+			{
+				Log.Error("[TreeDesireDeannoyance] HarmonyPatch_HaulAIUtility_HaulablePlaceValidator: unable to find injection point. This was likely due to a mod incompatibility; please report this to the mod author.");
 			}
 		}
 	}
