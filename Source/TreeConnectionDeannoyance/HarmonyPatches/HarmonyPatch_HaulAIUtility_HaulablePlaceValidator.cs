34339248f5e48069bad25af8bee13c600b4dfa77
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-16 23:51:17 -0400

    Update HarmonyPatch_HaulAIUtility_HaulablePlaceValidator.cs

--- Source/TreeConnectionDeannoyance/HarmonyPatches/HarmonyPatch_HaulAIUtility_HaulablePlaceValidator.cs
+++ Source/TreeConnectionDeannoyance/HarmonyPatches/HarmonyPatch_HaulAIUtility_HaulablePlaceValidator.cs
@@ -2,8 +2,8 @@
 using RimWorld;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection.Emit;
-using Verse;
 using Verse.AI;
 
 namespace Cerespirin.TreeDesireDeannoyance
