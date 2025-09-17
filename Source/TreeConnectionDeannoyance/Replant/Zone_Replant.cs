8607be4fd2ee5387978a29b6041746541bcc226d
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-16 20:25:48 -0400

    Update Zone_Replant.cs

--- Source/TreeConnectionDeannoyance/Replant/Zone_Replant.cs
+++ Source/TreeConnectionDeannoyance/Replant/Zone_Replant.cs
@@ -115,6 +115,7 @@ namespace Cerespirin.TreeDesireDeannoyance
 		};
 
 		//public bool enabled;
+		public bool allowCut = false;
 		public StorageSettings settings;
 		private static StorageSettings cachedFixedSettings;
 	}
