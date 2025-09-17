1563bdaf31abb1810075e4bb5f2bca54cece0ae1
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-17 02:31:06 -0400

    Update HarmonyPatch_HaulAIUtility_HaulablePlaceValidator.cs

--- Source/TreeConnectionDeannoyance/HarmonyPatches/HarmonyPatch_HaulAIUtility_HaulablePlaceValidator.cs
+++ Source/TreeConnectionDeannoyance/HarmonyPatches/HarmonyPatch_HaulAIUtility_HaulablePlaceValidator.cs
@@ -14,32 +14,34 @@ namespace Cerespirin.TreeDesireDeannoyance
 	{
 		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
 		{
-			LocalBuilder extractSetting = generator.DeclareLocal(typeof(bool));
-			object lastBranchLabel = null;
-			bool didTheThing = false;
+			LocalBuilder newLocal = generator.DeclareLocal(typeof(bool));
+			Label newLabel = generator.DefineLabel();
+			byte state = 0;
 
 			foreach (CodeInstruction instruction in instructions)
 			{
-				if (instruction.opcode == OpCodes.Brfalse_S)
+				if (state == 1 && instruction.opcode == OpCodes.Brfalse_S)
 				{
-					lastBranchLabel = instruction.operand;
+					state = 2;
+					yield return new CodeInstruction(OpCodes.Brtrue_S, newLabel);
+					yield return new CodeInstruction(OpCodes.Ldloc_S, newLocal.LocalIndex);
+					yield return new CodeInstruction(OpCodes.Isinst, typeof(Zone_Replant));
+					yield return instruction;
+					yield return new CodeInstruction(OpCodes.Nop).WithLabels(newLabel);
 				}
 				if (instruction.opcode == OpCodes.Isinst && (Type)instruction.operand == typeof(Zone_Growing))
 				{
-					didTheThing = true;
-					yield return new CodeInstruction(OpCodes.Stloc_S, extractSetting.LocalIndex);
+					state = 1;
+					yield return new CodeInstruction(OpCodes.Stloc_S, newLocal.LocalIndex);
+					yield return new CodeInstruction(OpCodes.Ldloc_S, newLocal.LocalIndex);
 					yield return instruction;
-					yield return new CodeInstruction(OpCodes.Brfalse_S, lastBranchLabel);
-					yield return new CodeInstruction(OpCodes.Ldloc_S, extractSetting.LocalIndex);
-					yield return new CodeInstruction(OpCodes.Isinst, typeof(Zone_Replant));
-					continue;
 				}
 				else
 				{
 					yield return instruction;
 				}
 			}
-			if (!didTheThing)
+			if (state < 2)
 			{
 				Log.Error("[TreeDesireDeannoyance] HarmonyPatch_HaulAIUtility_HaulablePlaceValidator: unable to find injection point. This was likely due to a mod incompatibility; please report this to the mod author.");
 			}
