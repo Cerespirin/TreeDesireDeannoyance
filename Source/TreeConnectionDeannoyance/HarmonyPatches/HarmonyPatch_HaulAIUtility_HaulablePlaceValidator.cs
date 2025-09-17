fbed72a21602f960c3402d096bdc7ac4ef117bfa
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-16 23:37:53 -0400

    Create HarmonyPatch_HaulAIUtility_HaulablePlaceValidator
    I can't believe how much functionality is hardcoded to the Zone_Growing type.

---
+++ Source/TreeConnectionDeannoyance/HarmonyPatches/HarmonyPatch_HaulAIUtility_HaulablePlaceValidator.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using Verse;
+using Verse.AI;
+
+namespace Cerespirin.TreeDesireDeannoyance
+{
+	[HarmonyPatch(typeof(HaulAIUtility), "HaulablePlaceValidator")]
+	public static class HarmonyPatch_HaulAIUtility_HaulablePlaceValidator
+	{
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+		{
+
+		}
+	}
+}
