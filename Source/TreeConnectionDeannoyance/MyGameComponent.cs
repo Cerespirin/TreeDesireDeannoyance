adacb5e17d881763f18c023eaf66caa7e6b13e00
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-18 06:40:48 -0400

    Remove autoReplantTrees setting
    Will be adding toggle functionality to individual zones soon.

--- Source/TreeConnectionDeannoyance/MyGameComponent.cs
+++ Source/TreeConnectionDeannoyance/MyGameComponent.cs
@@ -6,7 +6,6 @@ namespace Cerespirin.TreeDesireDeannoyance
 	public class MyGameComponent : GameComponent
 	{
 		public bool alwaysExtractTrees = false;
-		public bool autoReplantTrees = false;
 		public Dictionary<Gizmo, Thing> designatorOwners = new Dictionary<Gizmo, Thing>();
 
 		[System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Is part of a shipped public API.")]
@@ -15,7 +14,6 @@ namespace Cerespirin.TreeDesireDeannoyance
 		public override void ExposeData()
 		{
 			Scribe_Values.Look(ref alwaysExtractTrees, "TreeDesireDeannoyance_AlwaysExtractTrees");
-			Scribe_Values.Look(ref autoReplantTrees, "TreeDesireDeannoyance_AutoReplantTrees");
 		}
 	}
 }
--- Source/TreeConnectionDeannoyance/MyHelper.cs
+++ Source/TreeConnectionDeannoyance/MyHelper.cs
@@ -8,7 +8,6 @@ namespace Cerespirin.TreeDesireDeannoyance
 	public static class MyHelper
 	{
 		public static bool ExtractSetting => Current.Game.GetComponent<MyGameComponent>().alwaysExtractTrees;
-		public static bool ReplantSetting => Current.Game.GetComponent<MyGameComponent>().autoReplantTrees;
 		public static bool IsRelevantToTreeLovers(this Thing thing) => thing.def.plant.IsTree && thing.def.plant.treeLoversCareIfChopped;
 		public static bool IsRelevantToTreeLovers(this ThingDef def) => def.plant.IsTree && def.plant.treeLoversCareIfChopped;
 		//public static Area GetReplantArea(this Map map) => map.areaManager.GetLabeled("Replant");
