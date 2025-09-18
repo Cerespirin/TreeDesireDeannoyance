37afb2149de9b6a05e24daf7db115ab627602966
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-18 06:28:16 -0400

    Update MyGameComponent.cs

--- Source/TreeConnectionDeannoyance/MyGameComponent.cs
+++ Source/TreeConnectionDeannoyance/MyGameComponent.cs
@@ -9,6 +9,7 @@ namespace Cerespirin.TreeDesireDeannoyance
 		public bool autoReplantTrees = false;
 		public Dictionary<Gizmo, Thing> designatorOwners = new Dictionary<Gizmo, Thing>();
 
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Is part of a shipped public API.")]
 		public MyGameComponent(Game game) { }
 
 		public override void ExposeData()
