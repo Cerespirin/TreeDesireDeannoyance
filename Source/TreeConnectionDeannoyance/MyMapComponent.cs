c223895d885478f6814d6a153b4c87a764f2df6a
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-18 06:30:05 -0400

    Implemented DoReplant to MyMapComponent

--- Source/TreeConnectionDeannoyance/MyMapComponent.cs
+++ Source/TreeConnectionDeannoyance/MyMapComponent.cs
@@ -12,7 +12,35 @@ namespace Cerespirin.TreeDesireDeannoyance
 			// Long tick
 			if (map.IsHashIntervalTick(2000))
 			{
+				DoAutoReplant();
+			}
+		}
 
+		public void DoAutoReplant()
+		{
+			foreach (MinifiedTree thing in map.listerThings.ThingsMatching(ThingRequest.ForGroup(ThingRequestGroup.MinifiedThing)).OfType<MinifiedTree>())
+			{
+				if (InstallBlueprintUtility.ExistingBlueprintFor(thing) == null)
+				{
+					Gizmo gizmo = thing.GetGizmos().First(g => g.GetType() == typeof(Designator_Replant));
+					MyGameComponent component = Current.Game.GetComponent<MyGameComponent>();
+					Designator_Replant designator = (Designator_Replant)gizmo;
+
+					// I *still* can't believe that designators find their owners based on what the player has selected...
+					try
+					{
+						component.designatorOwners.Add(gizmo, thing);
+
+						IEnumerable<IntVec3> cells = MyHelper.GetReplantCells(thing, designator); //t.Map.GetReplantArea().ActiveCells.Where(c1 => designator.CanDesignateCell(c1));
+						if (!cells.Any()) { return; }
+
+						designator.DesignateSingleCell(cells.OrderBy(c2 => c2.DistanceToSquared(thing.Position)).First());
+					}
+					finally
+					{
+						component.designatorOwners.Remove(gizmo);
+					}
+				}
 			}
 		}
 	}
