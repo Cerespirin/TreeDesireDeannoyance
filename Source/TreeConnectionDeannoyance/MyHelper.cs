9c557bae03f24fcb9686a4a1b4e8963be6c8455b
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-20 09:18:23 -0400

    Created MyHelper.PossiblyChangeCutJobToHarvest
    Updated HarmonyPatch_JobMaker_MakeJob to use it. This is in preparation for the next commit...

--- Source/TreeConnectionDeannoyance/MyHelper.cs
+++ Source/TreeConnectionDeannoyance/MyHelper.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using RimWorld;
+using System.Collections.Generic;
 using System.Linq;
 using Verse;
+using Verse.AI;
 
 namespace Cerespirin.TreeDesireDeannoyance
 {
@@ -11,6 +13,19 @@ namespace Cerespirin.TreeDesireDeannoyance
 		public static bool IsRelevantToTreeLovers(this Thing thing) => thing.def.plant.IsTree && thing.def.plant.treeLoversCareIfChopped;
 		public static bool IsRelevantToTreeLovers(this ThingDef def) => def.plant.IsTree && def.plant.treeLoversCareIfChopped;
 		public static Area GetForageArea(this Map map) => map.areaManager.GetLabeled("Forage");
+
+		public static void PossiblyChangeCutJobToHarvest(ref Job job)
+		{
+			if (MyHelper.ExtractSetting && job.def == JobDefOf.CutPlant && job.targetA.Thing.IsRelevantToTreeLovers())
+			{
+				DesignationManager designationManager = job.targetA.Thing.Map.designationManager;
+				if (!designationManager.HasMapDesignationOn(job.targetA.Thing) || designationManager.DesignationOn(job.targetA.Thing, DesignationDefOf.ExtractTree) != null)
+				{
+					job.def = JobDefOf.ExtractTree;
+					job.ignoreDesignations = true;
+				}
+			}
+		}
 
 		public static readonly IEnumerable<ThingDef> cachedExtractables = DefDatabase<ThingDef>.AllDefs.Where(t => t.IsPlant && t.Minifiable);
 	}
