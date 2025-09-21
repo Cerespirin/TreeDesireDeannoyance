7be8ea1f17ccdad32e4948390bcd8084d48cab32
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-20 20:31:09 -0400

    Create HarmonyPatch_Designator_Map
    Would you believe designators get their map by assuming it's the same map the player is looking at? Well, yes, it's similar to how they get their owners. x.x

---
+++ Source/TreeConnectionDeannoyance/HarmonyPatches/HarmonyPatch_Designator_Map.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace Cerespirin.TreeDesireDeannoyance
+{
+	[HarmonyPatch(typeof(Designator), nameof(Designator.Map), MethodType.Getter)]
+	public static class HarmonyPatch_Designator_Map
+	{
+		public static bool Prefix(ref Map __result, Gizmo __instance)
+		{
+			MyGameComponent component = Current.Game.GetComponent<MyGameComponent>();
+
+			if (component.designatorOwners.TryGetValue(__instance, out Thing owner))
+			{
+				__result = owner.Map;
+				return false;
+			}
+			return true;
+		}
+	}
+}
