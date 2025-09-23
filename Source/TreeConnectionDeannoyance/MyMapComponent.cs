93dd502e0fbd10ed9956227f5a928b5207f7e24d
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-23 03:00:55 -0400

    Update MyMapComponent.cs

--- Source/TreeConnectionDeannoyance/MyMapComponent.cs
+++ Source/TreeConnectionDeannoyance/MyMapComponent.cs
@@ -64,8 +64,7 @@ namespace Cerespirin.TreeDesireDeannoyance
 
 		private static IEnumerable<IntVec3> GetReplantCells(Thing thing, Designator_Replant designator)
 		{
-			return from Zone_Replant zone in thing.Map.zoneManager.AllZones.OfType<Zone_Replant>().Where(z => z.allowReplant)
-					 where zone.settings.filter.Allows(thing)
+			return from Zone_Replant zone in thing.Map.zoneManager.AllZones.OfType<Zone_Replant>().Where(z => z.allowReplant && z.settings.filter.Allows(thing))
 					 from IntVec3 cell in zone.Cells
 					 where designator.CanDesignateCell(cell)
 					 select cell;
