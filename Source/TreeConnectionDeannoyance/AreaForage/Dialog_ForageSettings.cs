85337ab6ee351c18d73de71cf7bf548b8bebc151
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2025-09-23 06:56:51 -0400

    Update Dialog_ForageSettings.cs

--- Source/TreeConnectionDeannoyance/AreaForage/Dialog_ForageSettings.cs
+++ Source/TreeConnectionDeannoyance/AreaForage/Dialog_ForageSettings.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RimWorld;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +9,16 @@ using Verse;
 
 namespace Cerespirin.TreeDesireDeannoyance.AreaForage
 {
+	[StaticConstructorOnStartup]
 	public class Dialog_ForageSettings : Window
 	{
 		public override void DoWindowContents(Rect inRect)
 		{
 			throw new NotImplementedException();
 		}
+
+		private static readonly Texture2D ignoreIcon = ContentFinder<Texture2D>.Get("UI/Designators/Cancel");
+		private static readonly Texture2D harvestIcon = ContentFinder<Texture2D>.Get("UI/Designators/Harvest");
+		private static readonly Texture2D extractIcon = ContentFinder<Texture2D>.Get("UI/Designators/ExtractTree");
 	}
 }
